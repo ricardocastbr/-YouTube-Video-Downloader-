8debe0f91a6129c86159d5fa916808ba8785c519
ricardocastbr <ricardocastbr@gmail.com>
2025-05-16 18:58:57 -0300

    com foto

--- supradown/src/main.py
+++ supradown/src/main.py
@@ -5,6 +5,19 @@ import os
 def main(page: ft.Page):
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.appbar = ft.AppBar(
+        title=ft.Text('Supradown'),
+        center_title=True,
+        bgcolor=ft.Colors.BLACK54,
+        actions=[  
+            ft.Image(
+                src='assets/logo.png',
+                width=70,
+                height=70,
+                fit=ft.ImageFit.COVER,
+            )
+        ]
+    )
 
     entrada_usuario = ft.TextField(
         label="Link do vídeo:",
@@ -16,14 +29,24 @@ def main(page: ft.Page):
     )
 
 
+    button_usuario = ft.ElevatedButton(
+        text = "Baixar",
+        width=300,
+        bgcolor=ft.Colors.BLUE,
+        color=ft.Colors.WHITE,
+    )
+
+
+
     page.add(
         ft.Column(
             controls=[
                 entrada_usuario,
+                button_usuario,
             ]
         )
     )
     page.update()
 
 
-ft.app(main)
+ft.app(main, assets_dir="assets")
